6f28aaecfa671852b4890ac839b222f72354af76
HuangWeiChieh <weichiehhuang@gapp.nthu.edu.tw>
2022-02-16 14:18:31 +0800

    hide bus stop page

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
   <body>
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/bus-stop">Bus Stop</router-link> |
+      <!-- <router-link to="/bus-stop">Bus Stop</router-link> | -->
       <router-link to="/bus-line">Bus Line</router-link>
     </div>
     <router-view />
