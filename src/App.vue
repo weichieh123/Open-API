5e4f5cab9944fce9f991c5aacd18d0f36c9a3ff3
HuangWeiChieh <weichiehhuang@gapp.nthu.edu.tw>
2022-02-16 10:15:32 +0800

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
-  <header>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/bus-stop">Bus Stop</router-link> |
-      <router-link to="/bus-line">Bus Line</router-link>
-    </div>
+  <Fragment>
+    <header>
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/bus-stop">Bus Stop</router-link> |
+        <router-link to="/bus-line">Bus Line</router-link>
+      </div>
+    </header>
     <router-view />
-  </header>
+
+    <Footer/>
+    </Fragment>
 </template>
 
+<script>
+import Footer from './views/Footer.vue';
 
+export default {
+  name: 'App',
+  components: {
+    Footer
+  }
+}
+</script>
 
 <style lang="scss">
 @import './styles/bus.scss';
-
 </style>
